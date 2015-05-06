dc79efc51d6e99609e77015753d94f968672664f
Punnadi Gunarathna <punnadi@wso2.com>
2015-05-06 16:20:47 +0530

    adding datasource related methods

--- modules/integration/tests-integration/tests-scenarios/src/test/java/org/wso2/appfactory/tests/scenarios/DatasourceTestCase.java
+++ modules/integration/tests-integration/tests-scenarios/src/test/java/org/wso2/appfactory/tests/scenarios/DatasourceTestCase.java
@@ -1,21 +1,25 @@
 package org.wso2.appfactory.tests.scenarios;
 
+import com.google.gson.JsonObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.wso2.appfactory.integration.test.utils.AFConstants;
 import org.wso2.appfactory.integration.test.utils.AFIntegrationTest;
+import org.wso2.appfactory.integration.test.utils.AFIntegrationTestException;
 import org.wso2.appfactory.integration.test.utils.AFIntegrationTestUtils;
-import org.wso2.appfactory.integration.test.utils.rest.APIMIntegrationClient;
-import org.wso2.appfactory.integration.test.utils.rest.BuildRepoClient;
 import org.wso2.appfactory.integration.test.utils.rest.DatasourceClient;
 import org.wso2.carbon.automation.engine.annotations.ExecutionEnvironment;
 import org.wso2.carbon.automation.engine.annotations.SetEnvironment;
 
 /**
- * Created by punnadi on 4/29/15.
+ * Test case for datasources
  */
-public class DatasourceTestCase extends AFIntegrationTest{
+public class DatasourceTestCase extends AFIntegrationTest {
+    public static final String TEST_DATA_SOURCE = "testDataSrc122";
     private DatasourceClient datasourceClient = null;
 
     @BeforeClass(alwaysRun = true)
@@ -25,11 +29,38 @@ public class DatasourceTestCase extends AFIntegrationTest{
 
     @SetEnvironment(executionEnvironments = {ExecutionEnvironment.PLATFORM})
     @Test(description = "Add new datasource")
-    public void addNewDatasource() throws Exception {
-        datasourceClient.createDatasource("testDataSource", "Development", "jdbc:mysql://localhost:3306/test",
-                                          "My test datasource", "mysql", "root", "123456", false,
-                                          AFIntegrationTestUtils.getPropertyValue(AFConstants.DEFAULT_APP_APP_KEY));
+    public void testAddNewDatasource() throws AFIntegrationTestException {
+        JSONObject responseObj = datasourceClient.createDatasource(TEST_DATA_SOURCE, "Development",
+                                                                   "jdbc:mysql://localhost:3306/test12345",
+                                                                   "My test datasource", "mysql", "root",
+                                                                   "123456", false,
+                                                                   AFIntegrationTestUtils.getPropertyValue(
+                                                                           AFConstants.DEFAULT_APP_APP_KEY));
+        JSONArray results = (JSONArray) responseObj.get("Development");
+        Assert.assertEquals(Boolean.parseBoolean((String) results.get(0)), true);
+    }
 
+    //TODO:
+  /*  @SetEnvironment(executionEnvironments = {ExecutionEnvironment.PLATFORM})
+    @Test(description = "Update an existing datasource", dependsOnMethods = {"testAddNewDatasource"})
+    public void testUpdateExistingDatasource() throws AFIntegrationTestException {
+        JSONObject responseObj = datasourceClient.editDatasource(TEST_DATA_SOURCE, "Development",
+                                                                 "jdbc:mysql://localhost:3306/test12345",
+                                                                 "My edited test datasource", "mysql", "root",
+                                                                 "123123", true,
+                                                                 AFIntegrationTestUtils.getPropertyValue(
+                                                                         AFConstants.DEFAULT_APP_APP_KEY));
+    }
+*/
+
+    @SetEnvironment(executionEnvironments = {ExecutionEnvironment.PLATFORM})
+    @Test(description = "Delete a datasource", dependsOnMethods = {"testAddNewDatasource"})
+    public void testDeleteExistingDatasource() throws AFIntegrationTestException {
+        JSONObject responseObj = datasourceClient.deleteDatasource(TEST_DATA_SOURCE, "Development",
+                                                                   AFIntegrationTestUtils.getPropertyValue(
+                                                                           AFConstants.DEFAULT_APP_APP_KEY));
+        JSONObject results = (JSONObject) responseObj.get("Development");
+        Assert.assertEquals(results.getBoolean("error"), false);
     }
 
 
