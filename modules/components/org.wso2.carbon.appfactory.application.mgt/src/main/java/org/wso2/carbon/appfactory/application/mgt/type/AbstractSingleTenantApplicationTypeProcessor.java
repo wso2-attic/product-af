4fcb8fecae1a842c57fe51fbb28713b4dc8b203e
Hasitha Weerasinghe <hasithathilankaweerasinghe@gmail.com>
2015-05-20 10:00:12 +0530

    added retry count and retry delay in appfactory.xml and read those values in AbstractSingleTenantApplicationTypeProcessor class

--- modules/components/org.wso2.carbon.appfactory.application.mgt/src/main/java/org/wso2/carbon/appfactory/application/mgt/type/AbstractSingleTenantApplicationTypeProcessor.java
+++ modules/components/org.wso2.carbon.appfactory.application.mgt/src/main/java/org/wso2/carbon/appfactory/application/mgt/type/AbstractSingleTenantApplicationTypeProcessor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2005-2011 WSO2, Inc. (http://wso2.com)
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ */
 package org.wso2.carbon.appfactory.application.mgt.type;
 
 import org.apache.axiom.om.OMElement;
@@ -21,15 +36,17 @@ import java.util.Date;
 import java.util.Map;
 
 /**
- * Created by hasitha on 4/21/15.
+ * Base class for Single tenant applications
  */
-public abstract class AbstractSingleTenantApplicationTypeProcessor extends AbstractApplicationTypeProcessor{
+public abstract class AbstractSingleTenantApplicationTypeProcessor extends AbstractApplicationTypeProcessor {
 
     private static final Log log = LogFactory.getLog(AbstractSingleTenantApplicationTypeProcessor.class);
     private static final String PARAM_CARTRIDGE_IP = "{cartridgeIP}";
     private static final String ENVIRONMENT = "ApplicationDeployment.DeploymentStage";
     private static final String TENANT_MANAGEMENT_URL = "TenantMgtUrl";
     private static final String SYMBOL_AT = "@";
+    private static final String RETRY_COUNT = "AbstractSingleTenantApplicationTypeProcessor.RetryCount";
+    private static final String RETRY_DELAY = "AbstractSingleTenantApplicationTypeProcessor.RetryDelay";
 
     @Override
     public void doVersion(String applicationId, String targetVersion,
@@ -47,69 +64,54 @@ public abstract class AbstractSingleTenantApplicationTypeProcessor extends Abstr
     }
 
     @Override
-    public OMElement configureBuildJob(OMElement jobConfigTemplate,
-                                       Map<String, String> parameters, String projectType)
-            throws AppFactoryException {
+    public OMElement configureBuildJob(OMElement jobConfigTemplate, Map<String, String> parameters,
+                                       String projectType) throws AppFactoryException {
 
         if (jobConfigTemplate == null) {
             String msg =
                     "Class loader is unable to find the jenkins job configuration template for Maven application types";
             log.error(msg);
             throw new AppFactoryException(msg);
-
         }
         // configure repo data
         jobConfigTemplate = configureRepositoryData(jobConfigTemplate, parameters);
-
         // set the maven 3 config name
         setValueUsingXpath(jobConfigTemplate, AppFactoryConstants.MAVEN3_CONFIG_NAME_XAPTH_SELECTOR,
-                parameters.get(AppFactoryConstants.MAVEN3_CONFIG_NAME));
-
+                           parameters.get(AppFactoryConstants.MAVEN3_CONFIG_NAME));
         // Support for post build listener residing in jenkins server
         setValueUsingXpath(jobConfigTemplate,
-                AppFactoryConstants.PUBLISHERS_APPFACTORY_POST_BUILD_APP_EXTENSION_XPATH_SELECTOR,
-                parameters.get(AppFactoryConstants.APPTYPE_EXTENSION));
-
+                           AppFactoryConstants.PUBLISHERS_APPFACTORY_POST_BUILD_APP_EXTENSION_XPATH_SELECTOR,
+                           parameters.get(AppFactoryConstants.APPTYPE_EXTENSION));
         setValueUsingXpath(jobConfigTemplate,
-                AppFactoryConstants.PUBLISHERS_APPFACTORY_POST_BUILD_APP_ID_XPATH_SELECTOR,
-                parameters.get(AppFactoryConstants.APPLICATION_ID));
-
+                           AppFactoryConstants.PUBLISHERS_APPFACTORY_POST_BUILD_APP_ID_XPATH_SELECTOR,
+                           parameters.get(AppFactoryConstants.APPLICATION_ID));
         setValueUsingXpath(jobConfigTemplate,
-                AppFactoryConstants.PUBLISHERS_APPFACTORY_POST_BUILD_APP_VERSION_XPATH_SELECTOR,
-                parameters.get(AppFactoryConstants.APPLICATION_VERSION));
-
+                           AppFactoryConstants.PUBLISHERS_APPFACTORY_POST_BUILD_APP_VERSION_XPATH_SELECTOR,
+                           parameters.get(AppFactoryConstants.APPLICATION_VERSION));
         setValueUsingXpath(jobConfigTemplate,
-                AppFactoryConstants.PUBLISHERS_APPFACTORY_POST_BUILD_USERNAME_XPATH_SELECTOR,
-                parameters.get(AppFactoryConstants.APPLICATION_USER));
-
+                           AppFactoryConstants.PUBLISHERS_APPFACTORY_POST_BUILD_USERNAME_XPATH_SELECTOR,
+                           parameters.get(AppFactoryConstants.APPLICATION_USER));
         setValueUsingXpath(jobConfigTemplate,
-                AppFactoryConstants.PUBLISHERS_APPFACTORY_POST_BUILD_REPOFROM_XPATH_SELECTOR,
-                parameters.get(AppFactoryConstants.REPOSITORY_FROM));
-
+                           AppFactoryConstants.PUBLISHERS_APPFACTORY_POST_BUILD_REPOFROM_XPATH_SELECTOR,
+                           parameters.get(AppFactoryConstants.REPOSITORY_FROM));
         setValueUsingXpath(jobConfigTemplate, AppFactoryConstants.APPLICATION_TRIGGER_PERIOD,
-                parameters.get(AppFactoryConstants.APPLICATION_POLLING_PERIOD));
-
+                           parameters.get(AppFactoryConstants.APPLICATION_POLLING_PERIOD));
         return jobConfigTemplate;
     }
 
     @Override
-    public String getDeployedURL(String tenantDomain, String applicationId,
-                                 String applicationVersion, String stage)
-            throws AppFactoryException {
+    public String getDeployedURL(String tenantDomain, String applicationId, String applicationVersion,
+                                 String stage) throws AppFactoryException {
 
         String deployedUrl = StringUtils.EMPTY;
         try {
             String urlPattern = (String) this.properties.get(LAUNCH_URL_PATTERN);
-
             // get cartridge active IP
-            String cartridgeActiveIp =
-                    getCartridgeActiveIP(applicationId, applicationVersion, tenantDomain, stage,
-                            true);
-
+            String cartridgeActiveIp = getCartridgeActiveIP(applicationId, applicationVersion, tenantDomain, stage,
+                                                            true);
             // generate application url
             if (StringUtils.isNotBlank(urlPattern) && StringUtils.isNotBlank(cartridgeActiveIp)) {
                 urlPattern = urlPattern.replace(PARAM_CARTRIDGE_IP, cartridgeActiveIp);
-
                 deployedUrl = urlPattern;
             }
         } catch (AppFactoryException e) {
@@ -117,63 +119,52 @@ public abstract class AbstractSingleTenantApplicationTypeProcessor extends Abstr
             throw new AppFactoryException("Error while generating application url !");
         }
         return deployedUrl;
-
     }
 
-    public String getCartridgeActiveIP(String applicationId, String applicationVersion,
-                                        String tenantDomain, String stage,
-                                        boolean subscribeOnDeployment) throws AppFactoryException {
-
-
-        /*
-
-            move this method and the core util method to the super class to the super class
-
-            * implement a pole
-            * pole 3 times and throw exception
-            *
-
-         */
+    /**
+     * Get Cartridge Active IP
+     *
+     * @param applicationId
+     * @param applicationVersion
+     * @param tenantDomain
+     * @param stage
+     * @param subscribeOnDeployment
+     * @return
+     * @throws AppFactoryException
+     */
+    public String getCartridgeActiveIP(String applicationId, String applicationVersion, String tenantDomain,
+                                       String stage, boolean subscribeOnDeployment) throws AppFactoryException {
 
         AppFactoryConfiguration configuration = AppFactoryUtil.getAppfactoryConfiguration();
-        String serverURL = configuration
-                .getFirstProperty(ENVIRONMENT + XPATH_SEPERATOR + stage + XPATH_SEPERATOR +
-                        TENANT_MANAGEMENT_URL);
-        String userName = CarbonContext.getThreadLocalCarbonContext().getUsername() + SYMBOL_AT +
-                CarbonContext.getThreadLocalCarbonContext().getTenantDomain();
+        String serverURL = configuration.getFirstProperty(ENVIRONMENT + XPATH_SEPERATOR + stage
+                                                                  + XPATH_SEPERATOR + TENANT_MANAGEMENT_URL);
+        String userName = CarbonContext.getThreadLocalCarbonContext().getUsername() + SYMBOL_AT + CarbonContext
+                .getThreadLocalCarbonContext().getTenantDomain();
         String cartridgeAlias = getCartridgeAlias(applicationId, applicationVersion, tenantDomain,
-                subscribeOnDeployment);
+                                                  subscribeOnDeployment);
 
-        StratosRestService restService =
-                new StratosRestService(serverURL, userName, StringUtils.EMPTY);
+        StratosRestService restService = new StratosRestService(serverURL, userName, StringUtils.EMPTY);
         String clusterId = restService.getSubscribedCartridgeClusterId(cartridgeAlias);
         JDBCApplicationDAO jdbcApplicationDAO = JDBCApplicationDAO.getInstance();
 
         try {
-            int retryCount = 5; // Set this to appfactory xml
-            int retryDelay = 3; // Set this to appfactory xml
+            int retryCount = Integer.parseInt(AppFactoryUtil.getAppfactoryConfiguration().getFirstProperty(RETRY_COUNT);
+            int retryDelay = Integer.parseInt(AppFactoryUtil.getAppfactoryConfiguration().getFirstProperty(RETRY_DELAY);
             // retry retryCount times to process the request
             for (int i = 0; i < retryCount; i++) {
-                log.info("Trying for " + i + " time");
                 CartridgeCluster cartridgeCluster = jdbcApplicationDAO.getCartridgeClusterByClusterId(clusterId);
-
                 if (cartridgeCluster != null) {
-
                     DeployStatus currentStatus = jdbcApplicationDAO.getDeployStatus(applicationId, applicationVersion,
-                            stage, false, userName);
+                                                                                    stage, false, userName);
                     if (currentStatus.getLastDeployedStatus() == null) {
                         currentStatus.setLastDeployedStatus(AppFactoryConstants.DEPLOY_SUCCESS);
                         currentStatus.setLastDeployedTime(new Date().getTime());
-                        jdbcApplicationDAO
-                                .updateLastDeployStatus(applicationId, applicationVersion, stage, false,
-                                        null, currentStatus);
+                        jdbcApplicationDAO.updateLastDeployStatus(applicationId, applicationVersion, stage, false, null,
+                                                                  currentStatus);
                     }
-
                     return cartridgeCluster.getActiveIP();
                 }
-
                 Thread.sleep(1000 * retryDelay); // sleep retryDelay seconds
-
             }
 
         } catch (InterruptedException e) {
