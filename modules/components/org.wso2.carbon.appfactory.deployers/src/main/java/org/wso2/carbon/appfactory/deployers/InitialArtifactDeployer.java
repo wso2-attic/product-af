45c544635d60207471c72ac7e8af1e4ea3506b64
Danushka Fernando <danushkaf@wso2.com>
2015-07-13 15:05:05 +0530

    Code changes to support CAR apptype

--- modules/components/org.wso2.carbon.appfactory.deployers/src/main/java/org/wso2/carbon/appfactory/deployers/InitialArtifactDeployer.java
+++ modules/components/org.wso2.carbon.appfactory.deployers/src/main/java/org/wso2/carbon/appfactory/deployers/InitialArtifactDeployer.java
@@ -55,8 +55,8 @@ public class InitialArtifactDeployer extends AbstractStratosDeployer {
 //		}
 //	}
 
-	protected File[] getLastBuildArtifact(String path, String extension) throws AppFactoryException {
-		return getArtifact(path, extension);
+	protected File[] getLastBuildArtifact(String path, String extension, String stage, String applicationId) throws AppFactoryException {
+		return getArtifact(path, extension, stage, applicationId);
 	}
 
 	protected String getBaseRepoUrl() throws AppFactoryException {
