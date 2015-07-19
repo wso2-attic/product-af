4979091f6c4c740bb7699b1e8f71b24ed75ca01b
Ubuntu <ubuntu@puppet-master.novalocal>
2015-07-19 16:27:11 +0530

    mysql version

--- modules/puppet-manifests/appfactory/modules/privatepaas/manifests/init.pp
+++ modules/puppet-manifests/appfactory/modules/privatepaas/manifests/init.pp
@@ -23,11 +23,11 @@ class privatepaas (
   $deployment_code	= "privatepaas"
 
   $deployment_files =[ "apache-activemq-5.9.1-bin.tar.gz",
-                       "apache-stratos-4.1.0-SNAPSHOT.zip"
+                       "apache-stratos-4.1.0-SNAPSHOT.zip",
 #                      "apache-stratos-cartridge-agent-4.0.0-wso2v1-bin.zip",
 #                      "apache-stratos-load-balancer-4.0.0-wso2v1.zip",
 #                      "jdk-7u51-linux-x64.tar.gz",
-#                      "mysql-connector-java-5.1.29-bin.jar",
+                      "mysql-connector-java-5.1.29-bin.jar"
 #                      "wso2as-5.2.1.zip",
 #                      "wso2-private-paas-4.0.0-installer.zip"
                       ]
