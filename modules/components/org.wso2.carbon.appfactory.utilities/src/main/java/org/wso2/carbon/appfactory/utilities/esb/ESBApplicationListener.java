87fbdd8c6705bb47da42c4114a42124089509fdd
Danushka Fernando <danushkaf@wso2.com>
2015-08-07 19:24:57 +0530

    Refactoring + ESB resources changes

--- modules/components/org.wso2.carbon.appfactory.utilities/src/main/java/org/wso2/carbon/appfactory/utilities/esb/ESBApplicationListener.java
+++ modules/components/org.wso2.carbon.appfactory.utilities/src/main/java/org/wso2/carbon/appfactory/utilities/esb/ESBApplicationListener.java
@@ -15,53 +15,95 @@ package org.wso2.carbon.appfactory.utilities.esb;
  * limitations under the License.
  */
 
-import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.impl.builder.StAXOMBuilder;
-import org.apache.axis2.AxisFault;
-import org.apache.axis2.addressing.EndpointReference;
-import org.apache.axis2.client.ServiceClient;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.appfactory.common.AppFactoryConfiguration;
-import org.wso2.carbon.appfactory.common.AppFactoryConstants;
 import org.wso2.carbon.appfactory.common.AppFactoryException;
-import org.wso2.carbon.appfactory.common.util.AppFactoryUtil;
 import org.wso2.carbon.appfactory.core.ApplicationEventsHandler;
+import org.wso2.carbon.appfactory.core.RemoteRegistryService;
 import org.wso2.carbon.appfactory.core.apptype.ApplicationTypeManager;
 import org.wso2.carbon.appfactory.core.dto.Application;
 import org.wso2.carbon.appfactory.core.dto.UserInfo;
 import org.wso2.carbon.appfactory.core.dto.Version;
-import org.wso2.carbon.appfactory.utilities.security.authorization.RemoteAuthorizationMgtClient;
-import org.wso2.carbon.context.CarbonContext;
+import org.wso2.carbon.appfactory.core.runtime.RuntimeManager;
+import org.wso2.carbon.appfactory.utilities.internal.ServiceReferenceHolder;
 
-import javax.xml.stream.XMLStreamException;
-import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 
 public class ESBApplicationListener extends ApplicationEventsHandler {
 
     private static final Log log = LogFactory.getLog(ESBApplicationListener.class);
+	private static final String APPLICATION_TYPE_ESB = "esb-car";
+	private static final String ENDPOINT_MEDIA_TYPE = "application/vnd.wso2.esb.endpoint";
+	private static final String WSDL_MEDIA_TYPE = "WSDL";
+	private static final String ECHO_WSDL_RESOURCE_NAME = "echo.wsdl";
+	private static final String ECHO_ENDPOINT_RESOURCE_NAME = "EchoServiceEP.xml";
+	private static final String ECHO_ENDPOINT_RESOURCE_DESCRIPTION = "ESB default echo service endpoint";
+	private static final String ECHO_WSDL_RESOURCE_DESCRIPTION = "Echo wsdl to publish";
 
-    private static String sampleDbUrl = "jdbc:mysql://wso2admin.czrket1hdi7t.us-east-1.rds.amazonaws.com/testdb?autoReconnect=true";
-    private static String testUserName = "testuser";
-    private static String testUserPw = "testuser123";
-    private static String sampleDSName = "TestDS";
-	public static final String APPLICATION_TYPE_ESB = "dbs";
+	private RemoteRegistryService appfactoryRemoteRegistryService = null;
+	private File echoEndpointFile = null;
+	private File echoWsdlFile = null;
 
     public ESBApplicationListener(String identifier, int priority) {
         super(identifier, priority);
+	    appfactoryRemoteRegistryService = ServiceReferenceHolder.getInstance().getAppfactoryRemoteRegistryService();
+	    // Read files from resources of the component
+	    ClassLoader classLoader = getClass().getClassLoader();
+	    echoEndpointFile = new File(classLoader.getResource(ECHO_ENDPOINT_RESOURCE_NAME).getFile());
+	    echoWsdlFile = new File(classLoader.getResource(ECHO_WSDL_RESOURCE_NAME).getFile());
     }
 
     @Override
-    public void onCreation(Application application, String userName, String tenantDomain, boolean isUploadableAppType) throws AppFactoryException {
+    public void onCreation(Application application, String userName, String tenantDomain, boolean isUploadableAppType)
+		    throws AppFactoryException {
         if (APPLICATION_TYPE_ESB.equalsIgnoreCase(application.getType())) {
-            createSampleDataSource(application);
+
+
+	        String serverURL = RuntimeManager.getInstance().getRuntimeBean
+			        (ApplicationTypeManager.getInstance().getApplicationTypeBean(application.getType())
+			                               .getRuntimes()[0])
+	                                         .getServerURL();
+
+	        //Create resources
+	        try {
+		        appfactoryRemoteRegistryService.putRegistryProperty(serverURL, userName, application.getId(),
+		                                                            ECHO_ENDPOINT_RESOURCE_NAME,
+		                                                            FileUtils.readFileToString(echoEndpointFile),
+		                                                            ECHO_ENDPOINT_RESOURCE_DESCRIPTION,
+		                                                            ENDPOINT_MEDIA_TYPE, false);
+	        } catch (IOException e) {
+		        log.error("Reading endpoint file failed", e);
+		        //Not throwing exceptions since this will only fail launching the sample application
+	        }
+	        try {
+		        appfactoryRemoteRegistryService.putRegistryProperty(serverURL, userName, application.getId(),
+		                                                            ECHO_WSDL_RESOURCE_NAME,
+		                                                            FileUtils.readFileToString(echoWsdlFile),
+		                                                            ECHO_WSDL_RESOURCE_DESCRIPTION,
+		                                                            WSDL_MEDIA_TYPE, false);
+	        } catch (IOException e) {
+		        log.error("Reading wsdl file failed", e);
+		        //Not throwing exceptions since this will only fail launching the sample application
+	        }
         }
     }
 
     @Override
     public void onDeletion(Application application, String userName, String tenantDomain) throws AppFactoryException {
         if (APPLICATION_TYPE_ESB.equalsIgnoreCase(application.getType())) {
-            //deleteSampleDatasource();
+	        String serverURL = RuntimeManager.getInstance().getRuntimeBean
+			        (ApplicationTypeManager.getInstance().getApplicationTypeBean(application.getType())
+			                               .getRuntimes()[0])
+	                                        .getServerURL();
+	        //Sending the request with empty content for deletion
+	        appfactoryRemoteRegistryService.deleteRegistryResource(serverURL, userName, application.getId(),
+	                                                               ECHO_ENDPOINT_RESOURCE_NAME, "", "",
+	                                                               ENDPOINT_MEDIA_TYPE, false);
+	        appfactoryRemoteRegistryService.deleteRegistryResource(serverURL, userName, application.getId(),
+	                                                               ECHO_WSDL_RESOURCE_NAME, "", "",
+	                                                               WSDL_MEDIA_TYPE, false);
         }
 
     }
@@ -87,8 +129,8 @@ public class ESBApplicationListener extends ApplicationEventsHandler {
     }
 
     @Override
-    public void onVersionCreation(Application application, Version source, Version target, String tenantDomain, String userName)
-            throws AppFactoryException {
+    public void onVersionCreation(Application application, Version source, Version target, String tenantDomain,
+                                  String userName) throws AppFactoryException {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
@@ -100,14 +142,15 @@ public class ESBApplicationListener extends ApplicationEventsHandler {
     }
 
     @Override
-    public boolean hasExecuted(Application application, String userName, String tenantDomain) throws AppFactoryException {
-        ///// TODO implement method
-        return true;  //To change body of implemented methods use File | Settings | File Templates.
+    public boolean hasExecuted(Application application, String userName, String tenantDomain)
+		    throws AppFactoryException {
+	    //To change body of implemented methods use File | Settings | File Templates.
+        return true;
     }
 
     @Override
-    public void onFork(Application application, String userName, String tenantDomain, String version, String[] forkedUsers) throws AppFactoryException {
-        // TODO Auto-generated method stub
-
+    public void onFork(Application application, String userName, String tenantDomain, String version,
+                       String[] forkedUsers) throws AppFactoryException {
+	    //To change body of implemented methods use File | Settings | File Templates.
     }
 }
