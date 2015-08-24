d8cc8817f519238a54ec8de18210fad1fdbfac7a
Punnadi <punnadi@wso2.com>
2015-08-24 10:03:52 +0530

    stratos 4.1.1 changes added again after rebase with master

--- modules/puppet-manifests/appfactory/modules/privatepaas/files/stratos/stratos-installer/clean.sh
+++ modules/puppet-manifests/appfactory/modules/privatepaas/files/stratos/stratos-installer/clean.sh
@@ -23,23 +23,22 @@
 #  This script is for cleaning the host machine where one or more of the Stratos servers are run.
 # ----------------------------------------------------------------------------
 
-current_dir=$(dirname $0)
-source "$current_dir/conf/setup.conf"
+source "./conf/setup.conf"
 
 if [ "$UID" -ne "0" ]; then
 	echo ; echo "  You must be root to run $0.  (Try running 'sudo bash' first.)" ; echo 
 	exit 69
 fi
 
-function help() {
+function help {
     echo ""
-    echo "This script will clean Apache Stratos installation."
+    echo "Clean the host machine where one or more of the Stratos2 servers are run."
     echo "usage:"
-    echo "clean.sh -u <mysql username> -p <mysql password> -h <mysql hostname>"
+    echo "clean.sh -u <mysql username> -p <mysql password>"
     echo ""
 }
 
-while getopts u:p:h: opts
+while getopts u:p: opts
 do
   case $opts in
     u)
@@ -48,20 +47,14 @@ do
     p)
         mysql_pass=${OPTARG}
         ;;
-    h)
-        mysql_host=${OPTARG}
-        ;;
     *)
         help
-        exit 1
+        #exit 1
         ;;
   esac
 done
 
-# Set host to localhost if user has not specified a hostname
-mysql_host=${mysql_host:-localhost}
-
-function helpclean() {
+function helpclean {
     echo ""
     echo "Enter DB credentials if you need to clear Stratos DB"
     echo "usage:"
@@ -69,12 +62,12 @@ function helpclean() {
     echo ""
 }
 
-function clean_validate() {
-    if [[ -z $stratos_path ]]; then
+function clean_validate {
+    if [ -z $stratos_path ]; then
         echo "stratos_path is not set"
         exit 1
     fi
-    if [[ -z $log_path ]]; then
+    if [ -z $log_path ]; then
         echo "log_path is not set"
         exit 1
     fi
@@ -82,19 +75,32 @@ function clean_validate() {
 
 clean_validate
 if [[ ( -n $mysql_user && -n $mysql_pass ) ]]; then
-	read -p "Please confirm that you want to remove Apache Stratos databases, servers and logs [y/n] " answer
+	read -p "Please confirm that you want to remove stratos databases, servers and logs [y/n] " answer
 	if [[ $answer != y ]] ; then
     		exit 1
 	fi
 fi
-echo 'Stopping all java processes'
-killall java
+echo 'Stopping Carbon java processes'
+#killing carbon processes
+for pid in $(ps aux | grep "[o]rg.wso2.carbon.bootstrap.Bootstrap" | awk '{print $2}')
+do
+    echo "killing Carbon process $pid"
+    kill $pid
+done
+
+#killing activemq
+for pid in $(ps aux | grep "[a]pache-activemq" | awk '{print $2}')
+do
+    echo "killing ActiveMQ $pid"
+    kill $pid
+done
+
 echo 'Waiting for applications to exit'
-sleep 5
+sleep 15
 
 if [[ ( -n $mysql_user && -n $mysql_pass ) ]]; then
    echo 'Removing userstore database'
-   mysql -u $mysql_user -p$mysql_pass -h$mysql_host -e "DROP DATABASE IF EXISTS $userstore_db_schema;"
+   mysql -u $mysql_user -p$mysql_pass -e "DROP DATABASE IF EXISTS $userstore_db_schema;"
 fi
 
 if [[ -d $stratos_path/scripts ]]; then
@@ -102,6 +108,10 @@ if [[ -d $stratos_path/scripts ]]; then
    rm -rf $stratos_path/scripts
 fi
 
+if [[ -d $stratos_path ]]; then
+   echo 'Removing Stratos'
+   rm -rf $stratos_path/*
+fi
 
 echo 'Removing logs'
 rm -rf $log_path/*
