65bce45e6ae170b2eb9da1ac86a3f2eeb96ea3e3
Shenavi de Mel <shenavi@wso2.com>
2015-10-06 16:46:54 +0530

    Changing licence headers

--- modules/maven-archetype/maven-archetype-springapp/src/main/resources/archetype-resources/src/main/java/com/helloapp/controllers/HelloWorldController.java
+++ modules/maven-archetype/maven-archetype-springapp/src/main/resources/archetype-resources/src/main/java/com/helloapp/controllers/HelloWorldController.java
@@ -1,19 +1,20 @@
 /*
  * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * you may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 package com.helloapp.controllers;
 
 import org.springframework.stereotype.Controller;
@@ -24,14 +25,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  * Spring Controller Class
  */
-
 @Controller
 public class HelloWorldController {
 
 	/**
 	 * stores the string to be displayed specific to the methods being invoked
 	 */
-	String msg = "";
+	String msg;
 
 	/**
 	 * Method called when the request comes in the form of "/"
@@ -41,14 +41,11 @@ public class HelloWorldController {
 
 		//creating a new ModelAndView object and indicating the location of the view page
 		ModelAndView mav = new ModelAndView("index");
-
 		//the message to be passed
 		msg = "Hello World!!";
-
-       /*adding msg object to the created ModelAndView object,
-        the message will be assigned to the msg parameter
+	   /*adding msg object to the created ModelAndView object,
+	    the message will be assigned to the msg parameter
        */
-
 		mav.addObject("msg", msg);
 		return mav;
 	}
@@ -60,11 +57,8 @@ public class HelloWorldController {
 	public ModelAndView welcomePage(@PathVariable("username") String username) {
 
 		ModelAndView mav = new ModelAndView("index");
-
 		msg = "Welcome " + username + "!!";
-
 		mav.addObject("msg", msg);
-
 		return mav;
 	}
 }
