65a9ec0a882adadd54ea3ea5b81a625ccb5730f0
WSO2 Cloud <cloud@wso2.com>
2015-10-08 15:26:27 +0530

    Incorporate code review comments

---
+++ modules/components/org.wso2.carbon.appfactory.core/src/main/java/org/wso2/carbon/appfactory/core/workflow/WorkflowConstant.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2005-2013 WSO2, Inc. (http://wso2.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.appfactory.core.workflow;
+
+/**
+ * WorkflowDTO related constant values
+ */
+public class WorkflowConstant {
+
+    public static final String TENANT_CREATION = "WorkflowExetensions.WorkflowType.TenantCreation.Property.ClassName";
+    public static final String BPS_SERVER_URL = "WorkflowExetensions.BPS.Url";
+    public static final String WF_TYPE_AF_TENANT_CREATION = "AF_TENANT_CREATION";
+    public static final String DEFAULT_WORKFLOW_CLASS = "org.wso2.carbon.appfactory.core.workflow.TenantCreationDefaultWorkflowExecutor";
+
+}
