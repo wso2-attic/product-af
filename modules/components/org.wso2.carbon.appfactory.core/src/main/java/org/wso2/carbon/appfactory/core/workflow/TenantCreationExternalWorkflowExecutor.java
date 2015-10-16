dbccad397087fc83243b1c65d217c6a4b61ae821
WSO2 Cloud <cloud@wso2.com>
2015-10-16 10:18:54 +0530

    Changed licence headers

--- modules/components/org.wso2.carbon.appfactory.core/src/main/java/org/wso2/carbon/appfactory/core/workflow/TenantCreationExternalWorkflowExecutor.java
+++ modules/components/org.wso2.carbon.appfactory.core/src/main/java/org/wso2/carbon/appfactory/core/workflow/TenantCreationExternalWorkflowExecutor.java
@@ -1,17 +1,19 @@
 /*
- * Copyright 2005-2013 WSO2, Inc. (http://wso2.com)
+ *   Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
  */
 
 package org.wso2.carbon.appfactory.core.workflow;
@@ -60,8 +62,7 @@ public class TenantCreationExternalWorkflowExecutor implements WorkflowExecutor 
      * @param workflowDTO the tenant creation workflowDTO dto
      * @throws AppFactoryException AppFactoryException exception throws if not success the execution
      */
-    @Override
-    public void execute(WorkflowDTO workflowDTO) throws AppFactoryException {
+    @Override public void execute(WorkflowDTO workflowDTO) throws AppFactoryException {
 
         if (log.isDebugEnabled()) {
             String message = "Executing tenant creation external workflow for the tenant domain : " +
@@ -166,14 +167,11 @@ public class TenantCreationExternalWorkflowExecutor implements WorkflowExecutor 
                 "<usagePlan xmlns=\"http://wso2.org/bps/sample\">Demo</usagePlan>" +
                 "</p:CreateTenantRequest>";
 
-        String payLoadValue;
-        payLoadValue = MessageFormat.format(value, tenantCreationWorkflow.getTenantInfoBean().getAdmin(),
+        return MessageFormat.format(value, tenantCreationWorkflow.getTenantInfoBean().getAdmin(),
                 tenantCreationWorkflow.getTenantInfoBean().getFirstname(),
                 tenantCreationWorkflow.getTenantInfoBean().getLastname(),
                 tenantCreationWorkflow.getTenantInfoBean().getAdminPassword(), tenantCreationWorkflow.getTenantDomain(),
                 tenantCreationWorkflow.getTenantInfoBean().getTenantId(),
                 tenantCreationWorkflow.getTenantInfoBean().getEmail());
-
-        return payLoadValue;
     }
 }
