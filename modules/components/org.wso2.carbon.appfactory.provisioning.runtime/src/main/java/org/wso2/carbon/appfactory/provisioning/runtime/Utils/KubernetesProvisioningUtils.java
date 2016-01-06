a7d97dbc79597df3b9f7329d0b93e40e6db323cb
WSO2 Cloud <cloud@wso2.com>
2016-01-06 14:06:14 +0000

    revert auth details

--- modules/components/org.wso2.carbon.appfactory.provisioning.runtime/src/main/java/org/wso2/carbon/appfactory/provisioning/runtime/Utils/KubernetesProvisioningUtils.java
+++ modules/components/org.wso2.carbon.appfactory.provisioning.runtime/src/main/java/org/wso2/carbon/appfactory/provisioning/runtime/Utils/KubernetesProvisioningUtils.java
@@ -45,8 +45,8 @@ public class KubernetesProvisioningUtils {
 
         //todo need to modify config object with master credentials properly
         Config config = new Config();
-        config.setUsername("vagrant");
-        config.setPassword("vagrant");
+        //config.setUsername("vagrant");
+        //config.setPassword("vagrant");
         config.setNoProxy(new String[]{"https://192.168.19.249:8080"});
         config.setMasterUrl(KubernetesPovisioningConstants.KUB_MASTER_URL);
         config.setApiVersion(KubernetesPovisioningConstants.KUB_API_VERSION);
